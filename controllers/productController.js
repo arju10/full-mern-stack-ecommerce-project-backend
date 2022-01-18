b23f73fade25fb5152aa6fbc5b1ff026dcc8b022
arju10 <arjuarju71@gmail.com>
2022-01-18 15:56:38 +0600

    product delete

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,22 +1,46 @@
 const Product = require("../models/product");
 
-// find()=== SELECT * from x
-exports.getProducts =async (req, res, next) => {
+// Display all products => ("/api/v1/products") [method : "GET"]
+exports.getProducts = async (req, res, next) => {
   try {
+    // find()=== SELECT * from x
     const getAllProducts = await Product.find();
 
     res.status(200).json({
       success: true,
       // message: "This shows all products",
-      count:getAllProducts.length,
-      getAllProducts
+      count: getAllProducts.length,
+      getAllProducts,
     });
-    console.log("Number of Products available in Database : ",getAllProducts.length);
+    console.log(
+      "Number of Products available in Database : ",
+      getAllProducts.length
+    );
   } catch (error) {
     console.log(error.message);
   }
-
 };
+
+// Get Single Product Details => ("/api/v1/product/:id") [method:"GET"]
+exports.getSingleProduct = async (req, res, next) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if(!product){
+      return res.status(404).json({
+        success: false,
+        message : "Product not found"
+      })
+    }
+
+    res.status(200).json({
+      success: true,
+      product
+    })
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
 
 // Create new product => ("/api/v1/admin/product/new") [method : "POST"]
 // exports.newProduct = async(res, req, next) => {
@@ -28,9 +52,10 @@ exports.getProducts =async (req, res, next) => {
 //     });
 // };
 
-// Alternative new product => ("/api/v1/product/new") [method : 'Post']
+// create new product => ("/api/v1/product/new") [method : 'POST']  (Alternative)
 exports.newProduct = async (req, res, next) => {
   try {
+    // Saving Data to the Database
     const product = await Product.create(req.body);
     res.status(201).json({
       success: true,
@@ -45,6 +70,7 @@ exports.newProduct = async (req, res, next) => {
 
 // Update product => ("/api/v1/admin/product/:id") [method : "PUT"]
 exports.updateProduct = async (req, res, next) => {
+  // Finding the product
   let product = await Product.findById(req.params.id);
   if (!product) {
     return res.status(404).json({
@@ -52,7 +78,7 @@ exports.updateProduct = async (req, res, next) => {
       message: "Product isn't updated",
     });
   }
-
+  // Saving/Updating the product
   product = await Product.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
     runValidators: true,
@@ -64,4 +90,28 @@ exports.updateProduct = async (req, res, next) => {
     product,
     message: "Product is updated successfully",
   });
+};
+
+// Delete product from database => ("/api/v1/admin/product/:id") [method : "DELETE"]
+exports.deleteProduct = async (req, res, next) => {
+  try {
+    // Finding the product
+    const product = await Product.findById(req.params.id);
+    if(!product){
+      return res.status(404).json({
+        success: false,
+        message: "Product isn't found"
+      });
+    }
+
+    // Deleting the product
+    // product = await Product.remove();
+    product.remove();
+    res.status(200).json({
+      success: true,
+      message:"Product is Deleted Successfully!"
+    });
+  } catch (error) {
+    console.log(error);
+  }
 };
--- routes/product.js
+++ routes/product.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 
 
-const {getProducts, newProduct, updateProduct} = require("../controllers/productController");
+const {getProducts, newProduct, updateProduct, getSingleProduct, deleteProduct} = require("../controllers/productController");
 
 router.route("/products").get(getProducts);
+router.route("/product/:id").get(getSingleProduct);
 router.route("/admin/product/new").post(newProduct);
 router.route("/admin/product/:id").put(updateProduct);
+router.route("/admin/product/:id").delete(deleteProduct);
 
 module.exports = router;
