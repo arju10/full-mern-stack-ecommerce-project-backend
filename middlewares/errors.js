43b6ce0ecc02cf019a4d26b27988ebe7e5f1dca3
arju10 <arjuarju71@gmail.com>
2022-01-20 18:54:53 +0600

    create middleware

---
+++ middlewares/errors.js
@@ -0,0 +1,15 @@
+const ErrorHandler = require("../utils/errorHandler");
+
+module.exports = (err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.message = err.message || "Internal server error"
+    
+    if(process.env.NODE_ENV === "DEVELOPMENT"){
+        res.status(err.statusCode).json({
+            success: false,
+            error: err,
+            errMessage: err.message,
+            stack : err.stack
+        })
+    }
+}
