54f2c350c484efc56ccb52beadb8f4bf232d2d2a
arju10 <arjuarju71@gmail.com>
2022-01-21 09:24:06 +0600

    fix uncaughtException / Reference  error

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const dotenv = require("dotenv");
 const connectDatabase = require("./config/database");
 const { process_params } = require("express/lib/router");
 
+// Reference/Uncaught Error Handle
+process.on('uncaughtException', err => {
+    console.log(`ERROR :: ${err.message}`);
+    console.log("Uncaught Exception Error");
+    process.exit(1);
+});
+
 // Setting Env file
 dotenv.config({path:"./config/config.env"});
 
@@ -13,10 +20,11 @@ const server = app.listen(process.env.PORT, (req,res) => {
     console.log(`Server is running on http://localhost:${process.env.PORT} in ${process.env.NODE_ENV}`);
 });
 
+// Unhandled Promise Rejection Error Handle
 process.on('unhandledRejection', err => {
     console.log(`ERROR :: ${err.message}`);
     console.log("Shutting down the server due to unhandled promise rejection!");
     server.close(() => {
         process.exit(1);
     })
-})
+});
