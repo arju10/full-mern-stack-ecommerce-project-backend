085c347f71cc4dda922d0b8108c4df16ccbce9c7
arju10 <arjuarju71@gmail.com>
2022-01-21 18:43:37 +0600

    cast error handle but some ...

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -1,25 +1,39 @@
 const ErrorHandler = require("../utils/errorHandler");
 
 module.exports = (err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.message = err.message || "Internal server error"
-    
-    if(process.env.NODE_ENV === "DEVELOPMENT"){
-        res.status(err.statusCode).json({
-            success: false,
-            error: err,
-            errMessage: err.message,
-            stack : err.stack
-        })
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal server error";
+
+  if (process.env.NODE_ENV === "DEVELOPMENT") {
+    // Wrong mongoose ID
+    if (err.name === "castError") {
+      const message = `Resources not found. Invalid : ${err.path}`;
+
+      err = new ErrorHandler(message, 400);
     }
 
-    if(process.env.NODE_ENV === "PRODUCTION"){
-        error = {...err};
-        error.message = err.message;
+    res.status(err.statusCode).json({
+      success: false,
+      error: err,
+      errMessage: err.message,
+      stack: err.stack,
+    });
+  }
 
-        res.status(err.statusCode).json({
-            success : false,
-            error : error.message || "Internal Server Error!"
-        })
+  if (process.env.NODE_ENV === "PRODUCTION") {
+    error = { ...err };
+    error.message = err.message;
+
+    // Wrong mongoose ID
+    if (err.name === "castError") {
+      const message = `Resources not found. Invalid : ${err.path}`;
+
+      error = new ErrorHandler(message, 400);
     }
-}
+
+    res.status(err.statusCode).json({
+      success: false,
+      error: error.message || "Internal Server Error!",
+    });
+  }
+};
