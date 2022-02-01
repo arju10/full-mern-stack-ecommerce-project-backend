fb0473b0117a25479fc487c011b16d1a4664b1c2
arju10 <arjuarju71@gmail.com>
2022-02-01 12:09:41 +0600

    generate jwt token

--- models/user.js
+++ models/user.js
@@ -52,7 +52,7 @@ userSchema.pre("save", async function (next) {
     this.password = await bcrypt.hash(this.password, 10);
 })
 
-
+// Getting jwt token
 userSchema.methods.getJwtToken = function () {
     return jwt.sign({id : this.id}, process.env.JWT_SECRET, {
         expiresIn:process.env.JWT_EXPIRES_TIME
