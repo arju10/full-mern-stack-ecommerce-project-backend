1492641485d9fb5432e79eadb786ce0f136947e4
arju10 <arjuarju71@gmail.com>
2022-02-01 10:27:08 +0600

    encrypting passwod

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,8 +16,11 @@ exports.registerUser = catchAsyncErrors (async(req, res, next) => {
         }
     })
 
+    const token = user.getJwtToken()
+    
     res.status(201).json({
         success: true,
-        user
+        // user
+        token
     }); 
 });
--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -50,4 +51,11 @@ userSchema.pre("save", async function (next) {
     }
     this.password = await bcrypt.hash(this.password, 10);
 })
+
+
+userSchema.methods.getJwtToken = function () {
+    return jwt.sign({id : this.id}, process.env.JWT_SECRET, {
+        expiresIn:process.env.JWT_EXPIRES_TIME
+    })
+}
 module.exports = mongoose.model("user",userSchema);
