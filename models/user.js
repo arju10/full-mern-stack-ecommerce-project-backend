01d941b99176be8dcff96491782d4032eb745517
arju10 <arjuarju71@gmail.com>
2022-02-01 10:06:36 +0600

    encrypting password

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -42,4 +43,11 @@ const userSchema = new mongoose.Schema({
   restPasswordExpire: Date,
 });
 
+// Encrypting the Password
+userSchema.pre("save", async function (next) {
+    if(!this.isModified("password")){
+        next();
+    }
+    this.password = await bcrypt.hash(this.password, 10);
+})
 module.exports = mongoose.model("user",userSchema);
