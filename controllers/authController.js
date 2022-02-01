e7610ad1908ad4a3d490da2da03e1fc73ff51be0
arju10 <arjuarju71@gmail.com>
2022-02-01 09:30:16 +0600

    auth controller

---
+++ controllers/authController.js
@@ -0,0 +1,23 @@
+const User = require("../models/user");
+const ErrorHandler = require("../utils/errorHandler");
+const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
+
+// Register a user => (/api/v1/register) [method :""]
+exports.registerUser = catchAsyncErrors (async(req, res, next) => {
+    const {name, email, password} = req.body;
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+        avatar:{
+            public_id :"ID",
+            url : "URL"
+        }
+    })
+
+    res.status(201).json({
+        success: true,
+        user
+    });
+});
