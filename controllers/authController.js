e0e09f73b6424d092d1eb5f80fa3f5cdf485acba
arju10 <arjuarju71@gmail.com>
2022-02-03 09:22:20 +0600

    http only cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,61 +1,65 @@
 const User = require("../models/user");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
+const sendToken = require("../utils/jwtToken");
 
 // Register a user => (/api/v1/register) [method :"POST"]
-exports.registerUser = catchAsyncErrors (async(req, res, next) => {
-    const {name, email, password} = req.body;
+exports.registerUser = catchAsyncErrors(async (req, res, next) => {
+  const { name, email, password } = req.body;
 
-    const user = await User.create({
-        name,
-        email,
-        password,
-        avatar:{
-            public_id :"ID",
-            url : "URL"
-        }
-    })
+  const user = await User.create({
+    name,
+    email,
+    password,
+    avatar: {
+      public_id: "ID",
+      url: "URL",
+    },
+  });
 
-    const token = user.getJwtToken()
-    
-    res.status(201).json({
-        success: true,
-        // user
-        token
-    }); 
+  sendToken(user, 200, res);
+
+  // const token = user.getJwtToken()
+
+  // res.status(201).json({
+  //     success: true,
+  //     // user
+  //     token
+  // });
 });
 
-
 // Login a user => (/api/v1/login) [method : "POST"]
-exports.loginUser = catchAsyncErrors(async(req, res, next) => {
-    // console.log(req.body);
-    const {email, password} = req.body;
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+  // console.log(req.body);
+  const { email, password } = req.body;
 
-    // Checking if email and password is entered by user
-    if(!email || !password){
-        return next(new ErrorHandler("Please enter your email & password", 400));
-    }
+  // Checking if email and password is entered by user
+  if (!email || !password) {
+    return next(new ErrorHandler("Please enter your email & password", 400));
+  }
 
-    // Finding user in database
-    const user = await User.findOne({email}).select('+password');
-    // console.log(user);
+  // Finding user in database
+  const user = await User.findOne({ email }).select("+password");
+  // console.log(user);
 
-    // If user isn't existing in the Database
-    if(!user){
-        return next(new ErrorHandler("Invalid email or password", 401));
-    }
+  // If user isn't existing in the Database
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
 
-    // Check if password is correct or not
-    const isPasswordMatched = await user.comparePassword(password);
-    if(!isPasswordMatched){
-        return next(new ErrorHandler("Invalid email or password", 401));
-    }
-    
-    const token = user.getJwtToken()
-    
-    res.status(201).json({
-        success: true,
-        // user
-        token
-    }); 
-})
+  // Check if password is correct or not
+  const isPasswordMatched = await user.comparePassword(password);
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
+
+  sendToken(user, 200, res);
+
+  // const token = user.getJwtToken()
+
+  // res.status(201).json({
+  //     success: true,
+  //     // user
+  //     token
+  // });
+});
---
+++ utils/jwtToken.js
@@ -0,0 +1,18 @@
+const sendToken = (user, statusCode, res) => {
+  // Create Jwt Token
+  const token = user.getJwtToken();
+  // Options for cookie
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRES_TIME * 24 * 60 * 60 * 1000
+    ),
+
+    httpOnly: true,
+  };
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    token,
+    user,
+  });
+};
+module.exports = sendToken;
