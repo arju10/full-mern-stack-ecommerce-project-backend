5954d18bb3c2af00ec5627e938f55b849e2ae0f8
arju10 <arjuarju71@gmail.com>
2022-02-07 07:50:49 +0600

    update user password --auth controller -- err

--- controllers/authController.js
+++ controllers/authController.js
@@ -84,3 +84,16 @@ exports.getUserProfile = catchAsyncErrors(async (req, res, next) => {
     user,
   });
 });
+
+// Update / Change Password => /api/v1/password/update
+exports.updatePassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.user.id).select("+password");
+  // Check Previous user password
+  const isMatched = await user.comparePassword(req.body.oldPassword);
+  if (!isMatched) {
+    return next(new ErrorHandler("old Password is incorrect", 400));
+  }
+  user.password = req.body.password;
+  await user.save();
+  sendToken(user, 200, res);
+});
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { registerUser, loginUser, logout, getUserProfile} = require("../controllers/authController");
+const { registerUser, loginUser, logout, getUserProfile, updatePassword, } = require("../controllers/authController");
 const { isAuthenticatedUser } = require("../middlewares/auth");
 const router = express.Router();
 
@@ -7,4 +7,5 @@ router.route('/register').post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(logout);
 router.route("/me").get(isAuthenticatedUser,getUserProfile);
+router.route('/password/update').put(isAuthenticatedUser, updatePassword);
 module.exports = router;
